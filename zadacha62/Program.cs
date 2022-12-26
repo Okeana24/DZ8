1b07a7e3f305458aac52308671259d09a6ca631a
Olga <vvs.olga@yandex.ru>
2022-12-26 21:31:29 +0400

    zadacha62

---
+++ zadacha62/Program.cs
@@ -0,0 +1,38 @@
+﻿int a = 4;
+int[,] sqareMatrix = new int[a, a];
+
+int temp = 1;
+int b = 0;
+int B = 0;
+
+while (temp <= sqareMatrix.GetLength(0) * sqareMatrix.GetLength(1))
+{
+  sqareMatrix[b, B] = temp;
+  temp++;
+  if (b <= B + 1 && b + B < sqareMatrix.GetLength(1) - 1)
+    B++;
+  else if (b < B && b + B >= sqareMatrix.GetLength(0) - 1)
+    b++;
+  else if (b >= B && b + B > sqareMatrix.GetLength(1) - 1)
+    B--;
+  else
+    b--;
+}
+
+WriteArray(sqareMatrix);
+
+void WriteArray (int[,] array)
+{
+  for (int b = 0; b < array.GetLength(0); b++)
+  {
+    for (int B = 0; B < array.GetLength(1); B++)
+    {
+      if (array[b,B] / 10 <= 0)
+      Console.Write($" {array[b,B]} ");
+
+      else Console.Write($"{array[b,B]} ");
+    }
+    Console.WriteLine();
+  }
+}
+  
